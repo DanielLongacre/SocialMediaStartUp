859526546e23dfe1e825529c8d30f91938c84d6b
Daniel Longacre <danieljlongacre@gmail.com>
2023-11-01 14:31:51 -0400

    Added routes directories and files

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/socialMediaStartUp');
+// connect('mongodb://127.0.0.1:27017/socialMediaStartUp');
+connect(process.env.MONGOURI);
 
 module.exports = connection;
